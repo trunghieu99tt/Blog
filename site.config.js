81cb74f64f7fe1a1b72e214d721fa6f32f9b8135
Trung Hieu Nguyen <trunghieu99tt@gmail.com>
2025-10-11 22:11:57 +0700

    feat: update utterancesGitHubRepo

--- site.config.js
+++ site.config.js
@@ -36,7 +36,7 @@ module.exports = {
     imageCDNHost: null,
 
     // Utteranc.es comments via GitHub issue comments (optional)
-    utterancesGitHubRepo: 'trunghieu99tt/WhoWroteThisShit',
+    utterancesGitHubRepo: 'trunghieu99tt/Blog',
 
     // whether or not to enable support for LQIP preview images (optional)
     // NOTE: this requires you to set up Google Firebase and add the environment
